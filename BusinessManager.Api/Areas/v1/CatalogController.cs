cc737aa26d58427bda59170a5ad0dd525b5a77ab
Mukesh Murugan <iammukeshm@gmail.com>
2020-03-11 00:39:02 +0530

    api versioning

---
+++ BusinessManager.Api/Areas/v1/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessManager.Api.Controllers;
+using BusinessManager.Application.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessManager.Api.Areas.v1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class CatalogController : ApiController
+    {
+        [HttpGet("products")]
+        public async Task<IActionResult> GetAllProductsAsync()
+        {
+            //var products = await Mediator.Send(new GetAllProductsQuery());
+            string data = "a";
+            var result = Result<string>.Success("",data);
+            return Ok(result);
+        }
+    }
+}
---
+++ BusinessManager.Api/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
